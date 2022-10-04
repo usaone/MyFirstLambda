14329cc2673e87e5862f8913c2163b8877947d27
usaone <uddipmitra@gmail.com>
2022-10-03 23:00:15 -0400

    Add Json Serialization

--- Function.cs
+++ Function.cs
@@ -1,4 +1,5 @@
 using Amazon.Lambda.Core;
+using Newtonsoft.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -21,7 +22,8 @@ public class Function
 
     public string FunctionHandler2(object o, ILambdaContext context)
     {
-        LambdaLogger.Log($"FunctionHandler2: Type of object o is {o.GetType()}\n");
+        LambdaLogger.Log($"FunctionHandler2 begins\n");
+        LambdaLogger.Log("CONTEXT: " + JsonConvert.SerializeObject(context));
         LambdaLogger.Log($"Retuning");
         return $"Triggered from CloudWatch Rule";
     }
